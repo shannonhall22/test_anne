5c8d99a14a58ff95a892d449b1465159a8439a5c
shannonhall22 <shannonhall22@gmail.com>
2015-11-13 18:18:31 -0600

    minor change

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -6,7 +6,7 @@ Rails.application.configure do
     port:                 587,
     domain:               'agnesanne.com',
     user_name:            ENV['GMAIL_LOGIN'],
-    password:             'sobiscuitated29',
+    password:             ENV['GMAIL_PASSWORD'],
     authentication:       'plain',
     enable_starttls_auto: true  }
 
